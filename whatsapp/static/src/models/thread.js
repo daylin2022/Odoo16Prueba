41367665b7b0ca8984347b1eacb59fe263e8fb11
Daylin <dmurga@elitumgroup.com>
2024-05-21 12:45:24 -0500

    first commit

---
+++ whatsapp/static/src/models/thread.js
@@ -0,0 +1,53 @@
+/** @odoo-module **/
+
+import { registerPatch } from '@mail/model/model_core';
+import { one } from '@mail/model/model_field';
+import { clear } from '@mail/model/model_field_command';
+
+registerPatch({
+    name: 'Thread',
+    recordMethods: {
+        /**
+         * @override
+         */
+
+    },
+    fields: {
+        hasInviteFeature: {
+            compute() {
+                if (this.channel && this.channel.channel_type === 'whatsapp') {
+                    return true;
+                }
+                return this._super();
+            },
+        },
+        hasMemberListFeature: {
+            compute() {
+                if (this.channel && this.channel.channel_type === 'whatsapp') {
+                    return true;
+                }
+                return this._super();
+            },
+        },
+        isChatChannel: {
+            compute() {
+                if (this.channel && this.channel.channel_type === 'whatsapp') {
+                    return true;
+                }
+                return this._super();
+            },
+        },
+        /**
+         * If set, current thread is a livechat.
+         */
+        messagingAsPinnedWhatsapp: one('Messaging', {
+            compute() {
+                if (!this.messaging || !this.channel || this.channel.channel_type !== 'whatsapp' || !this.isPinned) {
+                    return clear();
+                }
+                return this.messaging;
+            },
+            inverse: 'pinnedWhatsapp',
+        }),
+    },
+});
